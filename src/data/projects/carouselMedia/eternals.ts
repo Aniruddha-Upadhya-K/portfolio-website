5bba89cfebc8c46b616b80169f459604d756bbae
Aniruddha Upadhya K <aniupadhyak1234@gmail.com>
2025-06-17 23:34:17 +0530

    feat: eternals details

---
+++ src/data/projects/carouselMedia/eternals.ts
@@ -0,0 +1,40 @@
+const contents = [
+  {
+    type: 'image', 
+    src: "/projects/eternals/home.webp", 
+    height: 708, 
+    width: 1000, 
+    widths: [240, 540, 720, 1136], 
+    sizes: "(max-width: 360px) 240px, (max-width: 720px) 540px, (max-width: 1024px) 720px, 1136px",
+    caption: "Eternals Home",
+  },
+  {
+    type: 'image', 
+    src: "/projects/eternals/gameplay.webp", 
+    height: 998, 
+    width: 710, 
+    widths: [240, 540, 720, 1136], 
+    sizes: "(max-width: 360px) 240px, (max-width: 720px) 540px, (max-width: 1024px) 720px, 1136px",
+    caption: "Gameplay",
+  },
+  {
+    type: 'image', 
+    src: "/projects/eternals/retry.webp", 
+    height: 1000, 
+    width: 706, 
+    widths: [240, 540, 720, 1136], 
+    sizes: "(max-width: 360px) 240px, (max-width: 720px) 540px, (max-width: 1024px) 720px, 1136px",
+    caption: "Game Over",
+  },
+  {
+    type: 'image', 
+    src: "/projects/eternals/highscore.webp", 
+    height: 993, 
+    width: 705, 
+    widths: [240, 540, 720, 1136], 
+    sizes: "(max-width: 360px) 240px, (max-width: 720px) 540px, (max-width: 1024px) 720px, 1136px",
+    caption: "Highscore Page",
+  },
+]
+
+export default contents;
